986e5cdc8f8e6eff626f7b8e94056b55addea8eb
ishopjapan <thoitran2107@gmail.com>
2022-09-12 16:00:15 +0700

    update

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -6,7 +6,10 @@ import com.codegym.service.ICustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -18,6 +21,43 @@ public class CustomerController {
     public String index(Model model) {
         List<Customer> customerList = customerService.findAll();
         model.addAttribute("customers", customerList);
-        return "view";
+        return "/index";
+    }
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "/create";
+    }
+    @PostMapping("/save")
+    public String save(Customer customer) {
+        customer.setId((int)(Math.random()*100));
+        customerService.save(customer);
+        return "redirect:/customer";
+    }
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id, Model model){
+        model.addAttribute("customer", customerService.findById(id));
+        return "/edit";
+    }
+    @PostMapping("/update")
+    public String update(Customer customer) {
+        customerService.update(customer.getId(), customer);
+        return "redirect:/customer";
+    }
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "/delete";
+    }
+    @PostMapping("/delete")
+    public String delete(Customer customer, RedirectAttributes attributes){
+        customerService.remove(customer.getId());
+        attributes.addFlashAttribute("success", "removed customer successfully!");
+        return "redirect:/customer";
+    }
+    @GetMapping("/{id}/view")
+    public String view(@PathVariable int id, Model model){
+        model.addAttribute("customer", customerService.findById(id));
+        return "/view";
     }
 }
